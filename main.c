bd09225776c4fc9df66196d7fc5bcf69d1c9ea20
Diogo Coelho <diogoamcoelho16@gmail.com>
2023-11-22 13:44:30 +0000

    Created the project in NetBeans

---
+++ main.c
@@ -0,0 +1,18 @@
+/**@file main.c 
+ * @author Diogo Coelho
+ * @version 1.0
+ * @brief 
+ * @date 22 de novembro de 2023, 13:34
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main() {
+
+    return (EXIT_SUCCESS);
+}
+
