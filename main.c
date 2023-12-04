185212c33e6b2661027c548d3c32836b6705b384
Diogo Coelho <diogoamcoelho16@gmail.com>
2023-12-04 15:47:13 +0000

    Documented the previously created function

--- main.c
+++ main.c
@@ -9,6 +9,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * @brief This function is a generalized way of generating a menu given a number of options and thier respective text
+ * @param num_op Number of options
+ * @param title Title of the menu
+ * @param op_array Array with the name of each option
+ * @return Returns the option selected by the user
+ */
 int menu(int num_op, char *title, char *op_array[]) {
 
     int op;
@@ -35,6 +42,9 @@ int menu(int num_op, char *title, char *op_array[]) {
         printf("-> ");
         scanf("%d", &op);
     }while(op < 0 || op > num_op);
+    
+    return op;
+    
 }
 
 /*
