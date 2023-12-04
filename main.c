ce5a8958a6ea442512b7fd53f6c0c61673b45cb4
Diogo Coelho <diogoamcoelho16@gmail.com>
2023-12-04 15:42:49 +0000

    Created general menu function

--- main.c
+++ main.c
@@ -7,6 +7,35 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+int menu(int num_op, char *title, char *op_array[]) {
+
+    int op;
+
+    do {
+        for (int i = 0; i < strlen(title) + 4; i++) {
+            printf("-");
+        }
+        printf("\n");
+
+        printf("  %s\n", title);
+
+        for (int i = 0; i < strlen(title) + 4; i++) {
+            printf("-");
+        }
+        printf("\n");
+
+        for (int i = 0; i < num_op; i++) {
+            printf("  %d -> %s\n", i + 1, op_array[i]);
+        }
+
+        printf("\n  0 -> Exit\n\n");
+
+        printf("-> ");
+        scanf("%d", &op);
+    }while(op < 0 || op > num_op);
+}
 
 /*
  * 
@@ -15,4 +44,3 @@ int main() {
 
     return (EXIT_SUCCESS);
 }
-
