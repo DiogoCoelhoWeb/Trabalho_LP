7e451109e2bedfa79afc6f4aa0776095c6cae56b
Diogo Coelho <diogoamcoelho16@gmail.com>
2023-12-07 10:53:24 +0000

    Created menu logic for the admin options
    Created the functions manage_comp_catalog() and manage_act_branches() to implement the menus for this features.
    
    Made some changes to the menu for a better user experience

--- menus.c
+++ menus.c
@@ -14,49 +14,108 @@
 
 #include "menus.h"
 
-
+//Main menu
 const char *PROFILE_OPTIONS[PROFILE_OPTION_SIZE] = {"Administrator", "User"};
-const char *ADMIN_OPTIONS[ADMIN_OPTION_SIZE] = {"Manage Comapnies Catalog", "Manage Activity Branches", "View Reports", "Go Back"};
-const char *USER_OPTIONS[USER_OPTION_SIZE] = {"Search Company", "Rate Company", "Comment Company","Go Back"};
 
-/**
- * @brief This function is a generalized way of generating a menu given a number of options and thier respective text
- * @param num_op Number of options
- * @param title Title of the menu
- * @param op_array Array with the name of each option
- * @return Returns the option selected by the user
- */
+//Profile menus
+const char *ADMIN_OPTIONS[ADMIN_OPTION_SIZE] = {"Manage Comapnies Catalog", "Manage Activity Branches", "View Reports", "Go Back"};
+const char *USER_OPTIONS[USER_OPTION_SIZE] = {"Search Company", "Rate Company", "Comment Company", "Go Back"};
+
+//Admin menu
+const char *MANAGE_CATALOG_OPTIONS[MANAGE_CATALOG_OPTION_SIZE] = {"Create Company", "Edit Company", "Remove Company", "Go Back"};
+const char *MANAGE_ACT_BRANCH_OPTIONS[MANAGE_ACT_BRANCH_OPTION_SIZE] = {"Create Activity Branch", "Edit Activity Branch", "Remove Activity Branch", "Go Back"};
+
+void print_menu(int num_op, char *title, const char *op_array[]) {
+    printf("%s ", title);
+
+    for (int i = 0; i < 39 - strlen(title); i++) {
+        printf("-");
+    }
+
+    printf("\n");
+
+    for (int i = 0; i < num_op; i++) {
+        printf("  %d -> %s\n", i + 1, op_array[i]);
+    }
+    
+    printf("  0 -> Exit\n");
+    printf("----------------------------------------\n\n");
+    printf("-> ");
+}
+
 int menu(int num_op, char *title, const char *op_array[]) {
 
     int op;
 
     do {
-        printf("%s ------------------------------\n", title);
-
-        for (int i = 0; i < num_op; i++) {
-            printf("  %d -> %s\n", i + 1, op_array[i]);
-        }
-
-        printf("  0 -> Exit\n");
-
-        for (int i = 0; i <= strlen(title); i++) {
-            printf("-");
-        }
-        printf("------------------------------\n\n");
-
-        printf("-> ");
+        print_menu(num_op, title, op_array);
         scanf("%d", &op);
-
     } while (op < 0 || op > num_op);
 
     return op;
 
 }
 
-/**
- * @brief This functiongenerates the menu for the profile ADMIN
- * @param main_op
- */
+/*ADMIN OPTIONS*/
+
+void manage_comp_catalog(int *main_op) {
+    int op;
+
+    do {
+        op = menu(MANAGE_CATALOG_OPTION_SIZE, "Admin - Companies Catalog", MANAGE_CATALOG_OPTIONS);
+
+        switch (op) {
+            case 1:
+                //TODO: insert_company();
+                break;
+
+            case 2:
+                //TODO: edit_company();
+                break;
+
+            case 3:
+                //TODO: remove_company();
+                break;
+
+            case 4:
+                break;
+
+            case 0:
+                *main_op = 0;
+                break;
+        }
+    } while (op != 0 && op != 4 && *main_op != 0);
+}
+
+void manage_act_branches(int *main_op) {
+    int op;
+
+    do {
+        op = menu(MANAGE_ACT_BRANCH_OPTION_SIZE, "Admin - Activity Branches", MANAGE_ACT_BRANCH_OPTIONS);
+
+        switch (op) {
+            case 1:
+                //TODO: insert_act_branch();
+                break;
+
+            case 2:
+                //TODO: edit_act_branch();
+                break;
+
+            case 3:
+                //TODO: remove_act_branch();
+                break;
+
+            case 4:
+                break;
+
+            case 0:
+                *main_op = 0;
+                break;
+        }
+    } while (op != 0 && op != 4 && *main_op != 0);
+}
+
 void admin_menu(int *main_op) {
     int op;
 
@@ -65,27 +124,31 @@ void admin_menu(int *main_op) {
 
         switch (op) {
             case 1:
-                //TODO: manage_comp_catalog();
+                manage_comp_catalog(main_op);
                 break;
+
             case 2:
-                //TODO: manage_act_branches();
+                manage_act_branches(main_op);
                 break;
+
             case 3:
                 //TODO: show_reports();
                 break;
+
             case 4:
                 break;
+
             case 0:
                 *main_op = 0;
                 break;
         }
-    } while (op != 0 && op != 4);
+    } while (op != 0 && op != 4 && *main_op != 0);
 }
 
-/**
- * @brief This function generates the menu for the profile USER
- * @param main_op
- */
+/*USER OPTIONS*/
+
+//TODO: Create the user options menu structure and program logic
+
 void user_menu(int *main_op) {
     int op;
 
@@ -96,24 +159,25 @@ void user_menu(int *main_op) {
             case 1:
                 //TODO: search_comp();
                 break;
+
             case 2:
                 //TODO: rate_comp();
                 break;
+
             case 3:
                 //TODO: comment_comp();
                 break;
+
             case 4:
                 break;
+
             case 0:
                 *main_op = 0;
                 break;
         }
-    } while (op != 0 && op != 4);
+    } while (op != 0 && op != 4 && *main_op != 0);
 }
 
-/**
- * @brief This function generates the application's main menu and waits for a user to choose an option to start
- */
 void main_menu() {
 
     int op;
@@ -125,12 +189,13 @@ void main_menu() {
             case ADMIN:
                 admin_menu(&op);
                 break;
+
             case USER:
                 user_menu(&op);
                 break;
+
             case 0:
                 break;
         }
     } while (op != 0);
-
 }
--- menus.h
+++ menus.h
@@ -15,9 +15,17 @@
 #ifndef MENUS_H
 #define MENUS_H
 
-#define PROFILE_OPTION_SIZE 2
-#define ADMIN_OPTION_SIZE   4
-#define USER_OPTION_SIZE    4
+//Main menu
+#define PROFILE_OPTION_SIZE           2
+
+//Profile menus
+#define ADMIN_OPTION_SIZE             4
+#define USER_OPTION_SIZE              4
+
+//Admin menu
+#define MANAGE_CATALOG_OPTION_SIZE    4
+#define MANAGE_ACT_BRANCH_OPTION_SIZE 4
+#define SHOW_REPORTS_OPTION_SIZE      4
 
 typedef enum {
     FALSE,
@@ -29,12 +37,48 @@ enum Profiles {
     USER
 };
 
+/**
+ * @brief This function is a generalized way of generating a menu given a number of options and thier respective text
+ * @param num_op Number of options
+ * @param title Title of the menu
+ * @param op_array Array with the name of each option
+ * @return Returns the option selected by the user
+ */
 int menu(int num_op, char *title, const char *op_array[]);
 
+/*ADMIN OPTIONS*/
+
+/**
+ * @brief This function generates the menu for the company catalog management
+ * @param main_op
+ */
+void manage_comp_catalog(int *main_op);
+
+/**
+ * @brief This function generates the menu for the activity branch management
+ * @param main_op
+ */
+void manage_act_branches(int *main_op);
+
+/*PROFILE MENUS*/
+
+/**
+ * @brief This function generates the menu for the profile ADMIN
+ * @param main_op
+ */
 void admin_menu(int *main_op);
 
+/**
+ * @brief This function generates the menu for the profile USER
+ * @param main_op
+ */
 void user_menu(int *main_op);
 
+/*MAIN MENU*/
+
+/**
+ * @brief This function generates the application's main menu and waits for a user to choose an option to start
+ */
 void main_menu();
 
 #endif /* MENUS_H */
