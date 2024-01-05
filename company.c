d5bfce75ba9712f96200b14d0ae08364c009947c
Diogo Coelho <diogoamcoelho16@gmail.com>
2024-01-05 15:41:39 +0000

    Implemented CRUD operations
    Implemented the necessary functions for  Creating, Reading, Updating and Deleting (CRUD) companies.

--- company.c
+++ company.c
@@ -10,7 +10,7 @@
 #include "company.h"
 #include "utilities.h"
 
-const char *EDIT_COMP_OPTIONS[EDIT_COMP_OPTION_SIZE] = {"Name", "Activity Branch", "Street", "Locality", "Postal Code", "Category", "Go Back"};
+const char *EDIT_COMP_OPTIONS[EDIT_COMP_OPTION_SIZE] = {"Name", "Activity Branch", "Street", "Locality", "Postal Code", "Category", "Enable/Disable", "Go Back"};
 
 char *print_category(int i) {
     switch (i) {
@@ -45,25 +45,24 @@ int read_cat(char *phrase) {
 
 void show_comp(Company company) {
 
-    printf("\n%s (%s) ----------------------------------------------------\n", company.name, company.active ? "Active" : "Inactive");
+    printf("\n%s (%s) ", company.name, company.active ? "Active" : "Inactive");
+    
+    for(int i = 0; i < 61 - (int) strlen(company.name) - (company.active == 1 ? 6 : 8); i++){
+        printf("-");
+    }
+    
+    printf("\n");
+    
     printf("  NIF                %d\n", company.NIF);
     printf("  Category           %s\n", print_category(company.category));
     printf("  Address            %s, %s, %s\n", company.address.street, company.address.locality, company.address.postal_code);
     printf("  Number of Ratings  %d\n", company.nClassis);
     printf("  Number of Comments %d\n", company.nComments);
 
-    for (int i = 0; i < (int) strlen(company.name); i++) {
-        printf("-");
-    }
-
-    if (!company.active) {
-        printf("--");
-    }
-
-    printf("--------------------------------------------------------------\n\n");
+    printf("-----------------------------------------------------------------\n\n");
 }
 
-void get_edit_string(Company *comp, char* str, char *phrase, char *menu_phrase, int size) {
+void get_edit_string(Company *comp, char *str, char *phrase, char *menu_phrase, int size) {
 
     int confirm;
 
@@ -72,7 +71,7 @@ void get_edit_string(Company *comp, char* str, char *phrase, char *menu_phrase, 
     do {
         getString(aux, size, phrase);
 
-        confirm = confirm_menu(menu_phrase, 0);
+        confirm = confirm_menu(menu_phrase, 1);
 
         if (confirm == 1) {
             strncpy(str, aux, size);
@@ -88,10 +87,10 @@ void get_edit_cat(Company *comp, Category *cat, char *phrase, char *menu_phrase)
     int cat_tmp;
 
     do {
-        
+
         cat_tmp = read_cat(phrase);
 
-        confirm = confirm_menu(menu_phrase, 0);
+        confirm = confirm_menu(menu_phrase, 1);
 
         if (confirm == 1) {
             *cat = cat_tmp;
@@ -99,6 +98,29 @@ void get_edit_cat(Company *comp, Category *cat, char *phrase, char *menu_phrase)
         }
 
     } while (confirm != 1 && confirm != 0);
+}
+
+void change_state(Company *company) {
+    if (company->active) {
+        printf("The company %s is enabled!\n", company->name);
+        if (confirm_menu("Are you sure you want to disbale the company?", 0) == 1) {
+            company->active = 0;
+        }
+
+        clear_screen();
+        show_comp(*company);
+        pause_exec();
+        return;
+    }
+
+    if (confirm_menu("Are you sure you want to enbale the company?", 0) == 1) {
+        company->active = 1;
+    }
+
+    clear_screen();
+    show_comp(*company);
+    pause_exec();
+
 }
 
 int search_comp(Companies companies, int NIF) {
@@ -117,11 +139,12 @@ int search_comp(Companies companies, int NIF) {
 
 void insert_comp(Companies *companies) {
     int NIF;
-    
-    clear_screen();
 
     Company *companiestmp;
 
+    clear_screen();
+
+    puts("Create Company -------------------------");
     NIF = getInt(100000000, 999999999, "Enter the companies NIF: ");
 
     if (search_comp(*companies, NIF) != -1) {
@@ -131,7 +154,6 @@ void insert_comp(Companies *companies) {
 
     if (companies->n_comp == companies->comp_mem_size) {
         companiestmp = (Company *) realloc(companies->companies, sizeof (Company) * (companies->comp_mem_size * COMPANY_MULT_FACTOR));
-
 
         if (companiestmp == NULL) {
             puts("Error creating company!");
@@ -155,69 +177,222 @@ void insert_comp(Companies *companies) {
     companies->companies[companies->n_comp].nClassis = 0;
     companies->companies[companies->n_comp].nComments = 0;
 
-    
-    show_comp(companies->companies[companies->n_comp]);
+    clear_screen();
 
+    show_comp(companies->companies[companies->n_comp]);
     companies->n_comp++;
-    
+
     pause_exec();
-    
+
 }
 
 void edit_comp(Companies *companies, int *main_op) {
     int NIF;
     int comp_position;
     int op;
+    int confirm_comp;
 
     clear_screen();
-    
+
     if (companies->n_comp == 0) {
-        puts("There are no companies stored!");
-        return;
-    }
-
-    NIF = getInt(100000000, 999999999, "Enter the companies NIF: ");
-
-    comp_position = search_comp(*companies, NIF);
-
-    if (comp_position == -1) {
-        puts("The company doesn't exist!");
+        puts("There are no companies stored!\n");
+        pause_exec();
         return;
     }
 
     do {
+        
+        clear_screen();
+        
+        NIF = getInt(100000000, 999999999, "Enter the companies NIF: ");
+        comp_position = search_comp(*companies, NIF);
+
+        if (comp_position == -1) {
+            puts("The company doesn't exist!\n");
+            pause_exec();
+            continue;
+        }
+        
+        show_comp(companies->companies[comp_position]);
+        
+        confirm_comp = confirm_menu("Are you sure you want to edit this company?", 1);
+        
+        if(confirm_comp == 0){
+            return;
+        }
+        
+    }while(confirm_comp != 1);
+
+    do {
+
+        clear_screen();
+
         op = menu(EDIT_COMP_OPTION_SIZE, "Edit Company", EDIT_COMP_OPTIONS);
 
         switch (op) {
             case 1:
+                clear_screen();
+                puts("Edit Company ---------------------------");
                 get_edit_string(&(companies->companies[comp_position]), companies->companies[comp_position].name, "Enter the new company's name: ", "Are you sure you want to keep the new name?", COMPANY_NAME_SIZE);
+                pause_exec();
                 break;
             case 2:
+                clear_screen();
+                puts("Edit Company ---------------------------");
                 //TODO: Activity Branch
+                pause_exec();
                 break;
             case 3:
+                clear_screen();
+                puts("Edit Company ---------------------------");
                 get_edit_string(&(companies->companies[comp_position]), companies->companies[comp_position].address.street, "Enter the company's new street: ", "Are you sure you want to keep the new street?", COMPANY_STREET_SIZE);
+                pause_exec();
                 break;
             case 4:
+                clear_screen();
+                puts("Edit Company ---------------------------");
                 get_edit_string(&(companies->companies[comp_position]), companies->companies[comp_position].address.locality, "Enter the company's new locality: ", "Are you sure you want to keep the new locality?", COMPANY_LOCALITY_SIZE);
                 break;
             case 5:
-                get_edit_string(&(companies->companies[comp_position]), companies->companies[comp_position].address.postal_code,"Enter the company's new postal_code: ", "Are you sure you want to keep the new postal_code?", 11);
+                clear_screen();
+                puts("Edit Company ---------------------------");
+                get_edit_string(&(companies->companies[comp_position]), companies->companies[comp_position].address.postal_code, "Enter the company's new postal_code: ", "Are you sure you want to keep the new postal_code?", 11);
+                pause_exec();
                 break;
             case 6:
+                clear_screen();
+                puts("Edit Company ---------------------------\n");
                 get_edit_cat(&(companies->companies[comp_position]), &(companies->companies[comp_position].category), "Enter the new company's category: ", "Are you sure you want to keep the new category?");
+                pause_exec();
                 break;
             case 7:
+                clear_screen();
+                puts("Edit Company ---------------------------\n");
+                change_state(companies->companies + comp_position);
+                break;
+            case 8:
                 break;
             case 0:
                 *main_op = 0;
                 break;
         }
-        
-    } while (op != 0 && op != 7);
+
+    } while (op != 0 && op != 8);
 
 }
 
-void remove_comp(Companies *companies){
+void remove_comp(Companies *companies) {
+
+    int NIF;
+    int comp_position;
+    Company *companiestmp;
+
     clear_screen();
+
+    if (companies->n_comp == 0) {
+        puts("\nThere are no companies stored!\n");
+        pause_exec();
+        return;
+    }
+
+    NIF = getInt(100000000, 999999999, "Enter the companies NIF: ");
+    comp_position = search_comp(*companies, NIF);
+
+    if (comp_position == -1) {
+        puts("\nThe company doesn't exist!\n");
+        pause_exec();
+        return;
+    }
+
+    show_comp(companies->companies[comp_position]);
+
+    if (confirm_menu("Are you sure you want to delete the company?", 0) == 2) {
+        return;
+    }
+
+    if (companies->companies[comp_position].nComments != 0) {
+
+        clear_screen();
+
+        if (companies->companies[comp_position].active) {
+            puts("--- Unable to delete the company because there are comments ---\nChanging state to Inactive!");
+            companies->companies[comp_position].active = 0;
+        } else {
+            puts("--- Unable to delete the company because there are comments ---\nThe company is already inactive!\nSkipping instruction!");
+        }
+        pause_exec();
+        return;
+    }
+
+    clear_screen();
+
+    puts("Deleting company...");
+
+    //Copying last company to the deleted company's location
+    companies->companies[comp_position].NIF = companies->companies[companies->n_comp - 1].NIF;
+    strncpy(companies->companies[comp_position].name, companies->companies[companies->n_comp - 1].name, COMPANY_NAME_SIZE);
+    companies->companies[comp_position].active = companies->companies[companies->n_comp - 1].active;
+    companies->companies[comp_position].act_code = companies->companies[companies->n_comp - 1].act_code;
+    companies->companies[comp_position].category = companies->companies[companies->n_comp - 1].category;
+
+    strncpy(companies->companies[comp_position].address.locality, companies->companies[companies->n_comp - 1].address.locality, COMPANY_LOCALITY_SIZE);
+    strncpy(companies->companies[comp_position].address.postal_code, companies->companies[companies->n_comp - 1].address.postal_code, 11);
+    strncpy(companies->companies[comp_position].address.street, companies->companies[companies->n_comp - 1].address.street, COMPANY_STREET_SIZE);
+
+
+    companies->companies[comp_position].classis = companies->companies[companies->n_comp - 1].classis;
+    companies->companies[comp_position].nClassis = companies->companies[companies->n_comp - 1].nClassis;
+
+    companies->companies[comp_position].comments = companies->companies[companies->n_comp - 1].comments;
+    companies->companies[comp_position].nComments = companies->companies[companies->n_comp - 1].nComments;
+
+    //Removing data from the last company's position
+    companies->companies[companies->n_comp - 1].NIF = 0;
+    companies->companies[companies->n_comp - 1].act_code = 0;
+    companies->companies[companies->n_comp - 1].active = 0;
+    companies->companies[companies->n_comp - 1].category = 0;
+    companies->companies[companies->n_comp - 1].nClassis = 0;
+    companies->companies[companies->n_comp - 1].nComments = 0;
+
+    strncpy(companies->companies[companies->n_comp - 1].name, "", COMPANY_NAME_SIZE);
+    strncpy(companies->companies[companies->n_comp - 1].address.locality, "", COMPANY_LOCALITY_SIZE);
+    strncpy(companies->companies[companies->n_comp - 1].address.postal_code, "", 11);
+    strncpy(companies->companies[companies->n_comp - 1].address.street, "", COMPANY_STREET_SIZE);
+
+    companies->companies[companies->n_comp - 1].classis = NULL;
+    companies->companies[companies->n_comp - 1].comments = NULL;
+
+    companies->n_comp--;
+
+    if (companies->n_comp < companies->comp_mem_size / 2) {
+        companiestmp = (Company *) realloc(companies->companies, sizeof (Company) * (companies->comp_mem_size / COMPANY_MULT_FACTOR));
+
+        if (companiestmp != NULL) {
+            companies->companies = companiestmp;
+            companies->comp_mem_size = companies->n_comp * COMPANY_MULT_FACTOR;
+        }
+    }
+
+    clear_screen();
+    puts("Company successfully deleted!\n");
+    pause_exec();
+}
+
+void list_comp(Companies companies) {
+
+    clear_screen();
+    
+    if (companies.n_comp == 0) {
+        puts("There are no companies stored!\n");
+        pause_exec();
+        return;
+    }
+
+    puts("List Companies --------------------------------------------------\n");
+    
+    for (int i = 0; i < companies.n_comp; i++) {
+        show_comp(companies.companies[i]);
+    }
+
+    pause_exec();
 }
--- menus.c
+++ menus.c
@@ -24,44 +24,54 @@ const char *ADMIN_OPTIONS[ADMIN_OPTION_SIZE] = {"Manage Comapnies Catalog", "Man
 const char *USER_OPTIONS[USER_OPTION_SIZE] = {"Search Company", "Rate Company", "Comment Company", "Go Back"};
 
 //Admin menu
-const char *MANAGE_CATALOG_OPTIONS[MANAGE_CATALOG_OPTION_SIZE] = {"Create Company", "Edit Company", "Remove Company", "Go Back"};
+const char *MANAGE_CATALOG_OPTIONS[MANAGE_CATALOG_OPTION_SIZE] = {"Create Company", "Edit Company", "Remove Company", "List Companies","Go Back"};
 const char *MANAGE_ACT_BRANCH_OPTIONS[MANAGE_ACT_BRANCH_OPTION_SIZE] = {"Create Activity Branch", "Edit Activity Branch", "Remove Activity Branch", "Reactivate Activity Branch", "Go Back"};
 
 /*ADMIN OPTIONS*/
 
-void manage_comp_catalog(int *main_op, Companies companies) {
+void manage_comp_catalog(int *main_op, Companies *companies) {
     int op;
 
     do {
+        
+        clear_screen();
+        
         op = menu(MANAGE_CATALOG_OPTION_SIZE, "Admin - Companies Catalog", MANAGE_CATALOG_OPTIONS);
 
         switch (op) {
             case 1:
-                insert_comp(&companies);
+                insert_comp(companies);
                 break;
 
             case 2:
-                edit_comp(&companies, main_op);
+                edit_comp(companies, main_op);
                 break;
 
             case 3:
-                //TODO: remove_company();
+                remove_comp(companies);
                 break;
 
             case 4:
+                list_comp(*companies);
+                break;
+                
+            case 5:
                 break;
 
             case 0:
                 *main_op = 0;
                 break;
         }
-    } while (op != 0 && op != 4 && *main_op != 0);
+    } while (op != 0 && op != 5 && *main_op != 0);
 }
 
 void manage_act_branches(int *main_op) {
     int op;
-
+    
     do {
+        
+        clear_screen();
+        
         op = menu(MANAGE_ACT_BRANCH_OPTION_SIZE, "Admin - Activity Branches", MANAGE_ACT_BRANCH_OPTIONS);
 
         switch (op) {
@@ -90,10 +100,13 @@ void manage_act_branches(int *main_op) {
     } while (op != 0 && op != 5 && *main_op != 0);
 }
 
-void admin_menu(int *main_op, Companies companies) {
+void admin_menu(int *main_op, Companies *companies) {
     int op;
-
+    
     do {
+        
+        clear_screen();
+        
         op = menu(ADMIN_OPTION_SIZE, "Administrator", ADMIN_OPTIONS);
 
         switch (op) {
@@ -121,10 +134,13 @@ void admin_menu(int *main_op, Companies companies) {
 
 /*USER OPTIONS*/
 
-void user_menu(int *main_op, Companies companies) {
+void user_menu(int *main_op, Companies *companies) {
     int op;
-
+    
     do {
+        
+        clear_screen();
+        
         op = menu(USER_OPTION_SIZE, "User", USER_OPTIONS);
 
         switch (op) {
@@ -152,15 +168,14 @@ void user_menu(int *main_op, Companies companies) {
 
 /*MAIN MENU*/
 
-void main_menu() {
+void main_menu(Companies *companies) {
 
     int op;
-
-    Companies companies;
-
-    companies.n_comp = 0;
     
     do {
+        
+        clear_screen();
+        
         op = menu(PROFILE_OPTION_SIZE, "Menu", PROFILE_OPTIONS);
 
         switch (op) {
